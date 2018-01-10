824ebcaea629a212a683087f0fc75264acf810c5
Jacqueline Lee <anginblu@gmail.com>
2018-01-10 20:34:54 +0000

    Done.

--- db/migrate/20150915202704_create_owners.rb
+++ db/migrate/20150915202704_create_owners.rb
@@ -1,11 +1,8 @@
 class CreateOwners < ActiveRecord::Migration
-  def up
+  def change
     create_table :owners do |t|
       t.string :name
     end
   end
-  
-  def down
-    drop_table :owners
-  end
+
 end
