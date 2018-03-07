775e5d27237b8c7a7357979f9dded5540ab2755e
ruthmesfun <ruth.mesfun@gmail.com>
2018-03-06 22:08:43 -0500

    Done.

--- db/migrate/20150915202821_add_column_to_cats.rb
+++ db/migrate/20150915202821_add_column_to_cats.rb
@@ -1,9 +1,5 @@
 class AddColumnToCats < ActiveRecord::Migration
-  def up
-    add_column :cats, :owner_id, :integer
-  end
-
-  def down
-    remove_column :cats, :owner_id
+  def change
+    add_column :cats, :owner_id, :intefer
   end
 end
