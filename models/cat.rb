1ba12dfaf16bf3e471a12f3bd024d69b8b08e973
Jul Jen <juliajen.prog@gmail.com>
2018-04-11 05:39:35 +0000

    Done.

--- models/cat.rb
+++ models/cat.rb
@@ -1,3 +1,3 @@
 class Cat < ActiveRecord::Base
-  belongs_to :owner
+  belongs_to :owner # Whenever we use a has_many we also have to use the belongs_to, model with the belongs_to association also has the foreign key
 end
--- models/owner.rb
+++ models/owner.rb
@@ -1,3 +1,3 @@
 class Owner < ActiveRecord::Base
-  has_many :cats
+  has_many :cats # Whenever we use a has_many we also have to use the belongs_to
 end
