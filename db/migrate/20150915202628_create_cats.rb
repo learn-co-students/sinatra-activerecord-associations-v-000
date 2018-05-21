50885a3ac1cb4003fe3e037efc3579b8102ec9d1
Sam. O <samrey2018@gmail.com>
2018-05-21 17:58:19 +0000

    Done.

--- db/migrate/20150915202628_create_cats.rb
+++ db/migrate/20150915202628_create_cats.rb
@@ -1,12 +1,12 @@
 class CreateCats < ActiveRecord::Migration
   def up
     create_table :cats do |t|
-      t.string :name
+      t.string  :name
       t.integer :age
-      t.string :breed
+      t.string  :breed
     end
   end
-  
+
   def down
     drop_table :cats
   end
--- models/cat.rb
+++ models/cat.rb
@@ -1,3 +1,5 @@
 class Cat < ActiveRecord::Base
+
+  # Whenever we use a has_many we also have to use the belongs_to (and vice-versa) in the other model.
   belongs_to :owner
 end
