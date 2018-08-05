5d0c42c02ec3936fc3acfda043b87aa02fd0fec3
Mary Jackson <jackson.marytherese@gmail.com>
2018-08-05 14:04:22 +0000

    Done.

--- db/migrate/20150915202821_add_column_to_cats.rb
+++ db/migrate/20150915202821_add_column_to_cats.rb
@@ -1,9 +1,9 @@
 class AddColumnToCats < ActiveRecord::Migration
-  def up
+  def change
     add_column :cats, :owner_id, :integer
   end
 
-  def down
-    remove_column :cats, :owner_id
-  end
+#  def down
+#    remove_column :cats, :owner_id
+#  end
 end
