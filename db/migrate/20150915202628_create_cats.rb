f6961e9cdb787c2b15163ea6149eeb1891373731
Lois Lee <lois.lee@me.com>
2019-02-20 21:12:59 +0000

    Done.

--- db/migrate/20150915202628_create_cats.rb
+++ db/migrate/20150915202628_create_cats.rb
@@ -1,13 +1,9 @@
 class CreateCats < ActiveRecord::Migration
-  def up
-    create_table :cats do |t|
-      t.string :name
-      t.integer :age
-      t.string :breed
-    end
-  end
-  
-  def down
-    drop_table :cats
-  end
+  def change
+     create_table :cats do |t|
+       t.string :name
+       t.integer :age
+       t.string :breed
+     end
+   end
 end
