acc4e3b80c855df0ac2bd9c40805b663796b070d
Luis Herrera <herrera.lrh@gmail.com>
2020-11-25 04:24:10 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150915202821) do
+ActiveRecord::Schema.define(version: 20201125041750) do
 
   create_table "cats", force: :cascade do |t|
     t.string  "name"
